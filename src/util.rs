6630b3e8d0267d2b22fd5949fc9da28856c79b01
SanmerDev <sanmer@sanmer.dev>
2024-06-17 15:26:46 +0800

    Optimize `Json`

--- src/util.rs
+++ src/util.rs
@@ -1,20 +1,24 @@
-use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 
-pub trait Json<'a> {
-    fn from_string(s: &'a str) -> Self;
+pub trait Json: Sized {
+    fn from_string(s: &str) -> Self;
     fn to_string(&self) -> String;
     fn to_string_pretty(&self) -> String;
 }
 
-impl<'a, T: Deserialize<'a> + Serialize> Json<'a> for T {
-    fn from_string(s: &'a str) -> Self {
+impl<T: DeserializeOwned + Serialize> Json for T {
+    #[inline]
+    fn from_string(s: &str) -> Self {
         serde_json::from_str(s).unwrap()
     }
 
+    #[inline]
     fn to_string(&self) -> String {
         serde_json::to_string(self).unwrap()
     }
 
+    #[inline]
     fn to_string_pretty(&self) -> String {
         serde_json::to_string_pretty(self).unwrap()
     }
